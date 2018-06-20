188c64eb9e25553c8afcc11e136053019b7ddc06
Kevin Xiao <xlzqwerty1@gmail.com>
2018-06-20 00:48:21 -0700

    first commit

---
+++ vue/src/App.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  #app
+    router-view
+</template>
+
+<style lang="scss">
+#app {
+  font-family: "Nunito", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  section.hero.is-fullheight
+    .hero-body
+      .container
+        h1.title Chat
+        h2.subtitle Hi {{ name }}, type and enter text below to start chatting.
+        .field
+          .control
+            input.input.is-rounded.is-medium(type="text", placeholder="Chat here")
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+
+@Component
+export default class Home extends Vue {
+
+  private name: string = ""
+
+}
+</script>
